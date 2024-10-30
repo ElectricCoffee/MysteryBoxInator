6b0e0bd8de4a4a8e44c2aa066757300f5b3fd44b
Niko Lepka <slench102+git@gmail.com>
2024-10-30 20:35:51 +0100

    Create Customer.kt

---
+++ src/Customer.kt
@@ -0,0 +1,5 @@
+class Customer(val name: String, val bggUserName: String?, val email: String, val address: AddressInfo) {
+    fun getBggCollection() {
+        throw NotImplementedError("not yet implemented")
+    }
+}
