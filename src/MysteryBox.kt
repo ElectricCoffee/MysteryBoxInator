67de7cadb0bba95e164f156f70f128a2c8fdb9d1
Niko Lepka <slench102+git@gmail.com>
2024-10-30 20:36:53 +0100

    Update MysteryBox.kt

--- src/MysteryBox.kt
+++ src/MysteryBox.kt
@@ -1,9 +1,11 @@
-class MysteryBox(val targetValue: Int) {
+// NB: targetValue is in pennies.
+open class MysteryBox(val targetValue: Int, val boxType: GameCategory) {
     var currentValue = 0
         private set;
 
     val items = mutableListOf<Game>();
 
+    // TODO: add box type to the inclusion process.... somehow
     fun addItem(item: Game): ItemAddStatus {
         val newCurrent = currentValue + item.value;
 
@@ -16,3 +18,7 @@ class MysteryBox(val targetValue: Int) {
         return ItemAddStatus.SUCCESS;
     }
 }
+
+class LargeMysteryBox(boxType: GameCategory) : MysteryBox(13500, boxType)
+class MediumMysteryBox(boxType: GameCategory) : MysteryBox(9000, boxType)
+class SmallMysteryBox(boxType: GameCategory) : MysteryBox(4500, boxType)
