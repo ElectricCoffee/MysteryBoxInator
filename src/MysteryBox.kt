abd0b93cca2b253e4c1c7f0cd48a20314a74bd0c
Niko Lepka <slench102+git@gmail.com>
2024-10-30 23:22:54 +0100

    add size info to mystery box as well as adding the 10% upper limit

--- src/MysteryBox.kt
+++ src/MysteryBox.kt
@@ -1,15 +1,23 @@
+import kotlin.math.roundToInt
+
 // NB: targetValue is in pennies.
-open class MysteryBox(val targetValue: Int, val boxType: GameCategory) {
+open class MysteryBox(val targetValue: Int, val boxType: GameCategory, val size: MysteryBoxSize) {
     var currentValue = 0
         private set;
 
     val items = mutableListOf<Game>();
 
+    val totalProfit: Int
+        get() = items.sumOf { it.profit }
+
+    private val targetValueUpperLimit = (targetValue + (targetValue * 0.1)).roundToInt()
+    private val targetValueLowerLimit = (targetValue - (targetValue * 0.05)).roundToInt() // find use for this.
+
     // TODO: add box type to the inclusion process.... somehow
     fun addItem(item: Game): ItemAddStatus {
-        val newCurrent = currentValue + item.value;
+        val newCurrent = currentValue + item.retailValue;
 
-        if (newCurrent > targetValue) {
+        if (newCurrent > targetValueUpperLimit) {
             return ItemAddStatus.EXCEEDS_TARGET_VALUE;
         }
 
@@ -19,6 +27,12 @@ open class MysteryBox(val targetValue: Int, val boxType: GameCategory) {
     }
 }
 
-class LargeMysteryBox(boxType: GameCategory) : MysteryBox(13500, boxType)
-class MediumMysteryBox(boxType: GameCategory) : MysteryBox(9000, boxType)
-class SmallMysteryBox(boxType: GameCategory) : MysteryBox(4500, boxType)
+// TODO: make these editable in config.
+class LargeMysteryBox(boxType: GameCategory)
+    : MysteryBox(13500, boxType, MysteryBoxSize.LARGE)
+
+class MediumMysteryBox(boxType: GameCategory)
+    : MysteryBox(9000, boxType, MysteryBoxSize.MEDIUM)
+
+class SmallMysteryBox(boxType: GameCategory)
+    : MysteryBox(4500, boxType, MysteryBoxSize.SMALL)
