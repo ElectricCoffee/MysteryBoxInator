9b5560681cf1bd6e5a733c34806419fb93cd73da
Niko Lepka <slench102+git@gmail.com>
2024-10-30 20:36:49 +0100

    update window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,6 +1,18 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class MainWindow extends JFrame {
-    private JTabbedPane tabbedPane1;
-    private JTable table1;
+    private JTabbedPane tabPane;
+    private JTable productTable;
+    private JPanel mainPanel;
+
+    public MainWindow() {
+        super("Mystery-Box-Inator");
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200, 800);
+        setLocationRelativeTo(null);
+
+        DefaultTableModel dtm = new DefaultTableModel(null, new Object[]{ "Title", "Value £", "Category, URL"});
+    }
 }
