6584f445df970e785911846486bb6d351f185f28
Niko Lepka <slench102+git@gmail.com>
2024-10-30 20:35:47 +0100

    Create AddressInfo.kt

---
+++ src/AddressInfo.kt
@@ -0,0 +1,3 @@
+class AddressInfo(val streetName: String, val number: String, val postCode: String, val state: String?, val country: String) {
+    constructor(streetName: String, number: String, postCode: String, country: String) : this(streetName, number, postCode, null, country)
+}
