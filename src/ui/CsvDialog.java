6123187a65b00c760d1881c84bc2a7e916f5d2e2
Niko Lepka <slench102+git@gmail.com>
2024-11-13 00:57:02 +0100

    add CSV Dialog and restructure the listeners a bit
    turns out the listeners didn't need to be entire class instances... Java doesn't make that very obvious with how pre-lambda lambdas were interface implementations, but seeing listeners in action in actual java code makes that rather obvious in hindsight.

---
+++ src/ui/CsvDialog.java
@@ -0,0 +1,63 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CsvDialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonAppend;
+    private JButton buttonOverwrite;
+    private JButton buttonCancel;
+    private JTextField pathField;
+    private JButton buttonOpen;
+
+    public CsvDialog() {
+        setTitle("Load CSV File");
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOverwrite);
+        setLocationRelativeTo(null);
+
+        buttonOpen.addActionListener(e -> onOpen());
+        buttonAppend.addActionListener(e -> onAppend());
+        buttonOverwrite.addActionListener(e -> onOverwrite());
+        buttonCancel.addActionListener(e -> onCancel());
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(
+                e -> onCancel(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOpen() {
+        // TODO open file dialog, then once a file was selected, populate the file path.
+    }
+
+    private void onAppend() {
+        // TODO: Use the file path to append data to the catalogue
+        dispose();
+    }
+
+    private void onOverwrite() {
+        // TODO: Use the file path to replace the catalogue
+        dispose();
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    public void openDialog() {
+        this.pack();
+        this.setVisible(true);
+    }
+}
--- src/ui/MenuBar.kt
+++ src/ui/MenuBar.kt
@@ -3,8 +3,6 @@ package ui
 import config.Config
 import config.configFolderPath
 import java.awt.Desktop
-import java.awt.event.ActionEvent
-import java.awt.event.ActionListener
 import java.io.File
 import java.lang.IllegalArgumentException
 import javax.swing.JMenu
@@ -18,22 +16,47 @@ class MenuBar(private val config: Config) : JMenuBar() {
 
         val loadCatalogueCsvItem = JMenuItem("Load Catalogue CSV...")
         loadCatalogueCsvItem.accessibleContext.accessibleDescription = "Lets you load the CSV with all the games"
+        loadCatalogueCsvItem.addActionListener { onLoadCsv() }
         menu.add(loadCatalogueCsvItem)
 
         val editConfigItem = JMenuItem("Open Config Folder...")
         editConfigItem.accessibleContext.accessibleDescription =
             "Opens the config file in your text editor of choice"
-        editConfigItem.addActionListener(ConfigFolderListener())
+        editConfigItem.addActionListener { onOpenConfigFolder() }
         menu.add(editConfigItem)
 
         val openOutputFolderItem = JMenuItem("Open Output Folder...")
         openOutputFolderItem.accessibleContext.accessibleDescription = "Opens the output folder on your computer"
-        openOutputFolderItem.addActionListener(OutputFolderListener(config))
+        openOutputFolderItem.addActionListener { onOpenOutputFolder(config) }
         menu.add(openOutputFolderItem)
 
         return menu;
     }
     init {
         this.add(fileMenu())
+    }
+
+    private fun onLoadCsv() {
+        // should open file dialogue, where the user can then add their csv file of choice
+        val dialog = CsvDialog()
+        dialog.openDialog()
+    }
+
+    private fun onOpenOutputFolder(config: Config) {
+        try {
+            val desktop = Desktop.getDesktop()
+            desktop.open(File(config.io.outputDirectory))
+        } catch (iae: IllegalArgumentException) {
+            TODO("Implement popup")
+        }
+    }
+
+    private fun onOpenConfigFolder() {
+        try {
+            val desktop = Desktop.getDesktop()
+            desktop.open(File(configFolderPath))
+        } catch (iae: IllegalArgumentException) {
+            TODO("Implement popup")
+        }
     }
 }
