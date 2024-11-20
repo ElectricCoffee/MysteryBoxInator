d9360be8197ca3f66080ff2a51ee12d4338faa71
Niko Lepka <slench102+git@gmail.com>
2024-11-20 23:11:28 +0100

    Fix potential error in pickRandom when list is empty

--- src/util/RandUtils.kt
+++ src/util/RandUtils.kt
@@ -4,8 +4,9 @@ import kotlin.random.Random
 
 object RandUtils {
     // todo: make a random picker util
-    fun <A>pickRandom(lst: List<A>): A {
+    fun <A>pickRandom(lst: List<A>): A? {
         val len = lst.count()
+        if (len == 0) return null
         val index = Random.nextInt(len)
         val outputValue = lst[index]
         return outputValue
