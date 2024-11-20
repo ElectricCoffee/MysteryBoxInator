dcd0ce47f4a8a43c226309829351dd9f3c7d0d2c
Niko Lepka <slench102+git@gmail.com>
2024-11-20 23:10:49 +0100

    make MysteryBox a simple data class and remove the old assembler

--- src/mysteryBox/MysteryBox.kt
+++ src/mysteryBox/MysteryBox.kt
@@ -1,34 +1,17 @@
 package mysteryBox
 
+import Budget
 import config.Config
 import Game
 import GameCategory
-import ItemAddStatus
 import java.math.BigDecimal
 
-// NB: targetValue is in pennies.
-open class MysteryBox(config: Config, val targetValue: BigDecimal, val boxType: GameCategory) {
-    var currentValue = BigDecimal.ZERO
-        private set;
+data class MysteryBox(val targetValue: BigDecimal, val boxType: GameCategory, val budgetStatus: Budget) {
+    val items = listOf<Game>();
 
-    val items = mutableListOf<Game>();
+    val totalValue: BigDecimal
+        get() = items.sumOf { it.retailValue }
 
     val totalProfit: BigDecimal
         get() = items.sumOf { it.profit }
-
-    private val targetValueUpperLimit = (targetValue + (targetValue * config.thresholds.upperAsFraction))
-    private val targetValueLowerLimit = (targetValue - (targetValue * config.thresholds.lowerAsFraction)) // find use for this.
-
-    // TODO: add box type to the inclusion process.... somehow
-    fun addItem(item: Game): ItemAddStatus {
-        val newCurrent = currentValue + item.retailValue;
-
-        if (newCurrent > targetValueUpperLimit) {
-            return ItemAddStatus.EXCEEDS_TARGET_VALUE;
-        }
-
-        items.add(item);
-        currentValue = newCurrent
-        return ItemAddStatus.SUCCESS;
-    }
 }
