1de6689c500b519769d52049c8aa619793c15d16
Niko Lepka <slench102+git@gmail.com>
2024-11-28 18:06:14 +0100

    refactor mainwindow

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -2,7 +2,6 @@ package ui;
 
 import catalogue.Catalogue;
 import config.Config;
-import errors.CsvParsingException;
 import io.Filing;
 import ui.util.CsvUtils;
 
@@ -10,19 +9,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.dnd.*;
-import java.io.File;
 import java.io.IOException;
 import java.math.RoundingMode;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-
-import static config.ConfigKt.*;
 
 public class MainWindow extends JFrame {
     private JTabbedPane tabPane;
@@ -39,28 +27,21 @@ public class MainWindow extends JFrame {
 
     public MainWindow() {
         super("Mystery-Box-Inator");
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(1200, 800);
 
         try {
-            setContentPane(mainPanel);
             Config config = Filing.createConfig();
             Catalogue catalogue = Filing.readWorkingCopy(config);
-            var dtm = configTable();
-            setJMenuBar(new MenuBar(config, catalogue, dtm));
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(1200, 800);
-            setLocationRelativeTo(null);
-            productTable.setModel(dtm);
-
-            new DropTarget(this, new CsvDropListener(this, config, catalogue, dtm));
-            dtm.addTableModelListener((e) -> {
-                numberOfGamesLoaded.setText(Integer.toString(catalogue.getCountGames()));
-                totalStockLabel.setText(Integer.toString(catalogue.getCountTotalInventory()));
-                totalProfitLabel.setText("£" + catalogue.getCatalogueProfit().setScale(2, RoundingMode.HALF_UP));
-            });
+            var configDtm = configCatalogueTable(config, catalogue);
+            configMysteryBoxTable();
+            setJMenuBar(new MenuBar(config, catalogue, configDtm));
 
             // populate the table with the catalogue's contents
             if (!catalogue.getGamesList().isEmpty()) {
-                CsvUtils.populateTable(config, catalogue, dtm);
+                CsvUtils.populateTable(config, catalogue, configDtm);
             }
 
         } catch (IOException ioe) {
@@ -72,10 +53,30 @@ public class MainWindow extends JFrame {
 
     void showErrorDialog(String message, String title) {
         JOptionPane.showMessageDialog(this, message, title, JOptionPane.ERROR_MESSAGE);
-
     }
 
-    DefaultTableModel configTable() {
-        return new DefaultTableModel(null, new Object[]{"Name", "Quantity", "Type", "Rarity", "Url", "Paste-Ups?", "Raw Cost", "Retail Price", "Item Profit", "Total Profit"});
+    DefaultTableModel configCatalogueTable(Config config, Catalogue catalogue) {
+        var dtm = new DefaultTableModel(null,
+                new Object[]{"Name", "Quantity", "Type", "Rarity", "Url", "Paste-Ups?", "Raw Cost", "Retail Price", "Item Profit", "Total Profit"});
+
+        productTable.setModel(dtm);
+
+        new DropTarget(this, new CsvDropListener(this, config, catalogue, dtm));
+        dtm.addTableModelListener((e) -> {
+            numberOfGamesLoaded.setText(Integer.toString(catalogue.getCountGames()));
+            totalStockLabel.setText(Integer.toString(catalogue.getCountTotalInventory()));
+            totalProfitLabel.setText("£" + catalogue.getCatalogueProfit().setScale(2, RoundingMode.HALF_UP));
+        });
+
+        return dtm;
+    }
+
+    DefaultTableModel configMysteryBoxTable() {
+        var dtm = new DefaultTableModel(null,
+                new Object[] {"Id", "Items", "Type", "Sell Price", "Budget", "Budget Status"});
+
+        mysteryBoxTable.setModel(dtm);
+
+        return dtm;
     }
 }
