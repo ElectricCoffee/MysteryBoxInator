4d9dc3ccca8dd2a0d8723ad2f6d840e8794d8ce5
Niko Lepka <slench102+git@gmail.com>
2024-11-29 20:11:08 +0100

    add toTableArray function to MysteryBox

--- src/mysteryBox/MysteryBox.kt
+++ src/mysteryBox/MysteryBox.kt
@@ -3,12 +3,30 @@ package mysteryBox
 import Budget
 import Game
 import GameCategory
+import util.NumUtils
 import java.math.BigDecimal
+import java.util.*
 
-data class MysteryBox(val items: List<Game>, val targetValue: BigDecimal, val boxType: GameCategory, val budgetStatus: Budget = Budget.OnBudget) {
+data class MysteryBox(val id: UUID, val items: List<Game>, val targetValue: BigDecimal, val boxType: GameCategory, val budgetStatus: Budget = Budget.OnBudget) {
+    constructor(items: List<Game>, targetValue: BigDecimal, boxType: GameCategory, budgetStatus: Budget)
+            : this(UUID.randomUUID(), items, targetValue, boxType, budgetStatus)
+
     val totalValue: BigDecimal
         get() = items.sumOf { it.retailValue }
 
     val totalProfit: BigDecimal
         get() = items.sumOf { it.profit }
+
+    fun toTableArray(): Array<Any> {
+//        arrayOf<Any>("Id", "Items", "Type", "Sell Price", "Budget", "Budget Status"));
+
+        return arrayOf(
+            id.toString(),
+            items.joinToString(", ") { it.title },
+            boxType.toString(),
+            NumUtils.asPrice(items.sumOf { it.retailValue }),
+            NumUtils.asPrice(targetValue),
+            budgetStatus.toPercentString()
+        )
+    }
 }
