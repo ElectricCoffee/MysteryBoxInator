6be6f86a72a6ae88d58505740dd0974966206448
Niko Lepka <slench102+git@gmail.com>
2024-11-29 20:10:24 +0100

    add asPrice to numUtiils

--- src/util/NumUtils.kt
+++ src/util/NumUtils.kt
@@ -1,9 +1,12 @@
 package util
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 object NumUtils {
 
     /** Converts a number between 0 and 100 to a number between 0 and 1 */
     fun toFraction(bigDecimal: BigDecimal) = bigDecimal / BigDecimal(100)
+
+    fun asPrice(num: BigDecimal) = "£" + num.setScale(2, RoundingMode.HALF_UP).toString()
 }
