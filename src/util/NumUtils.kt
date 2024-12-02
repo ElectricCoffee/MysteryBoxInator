395a47c263e43b3135b0d3873ad212c8770090b0
Niko Lepka <slench102+git@gmail.com>
2024-12-02 20:52:10 +0100

    fix toFraction

--- src/util/NumUtils.kt
+++ src/util/NumUtils.kt
@@ -6,7 +6,8 @@ import java.math.RoundingMode
 object NumUtils {
 
     /** Converts a number between 0 and 100 to a number between 0 and 1 */
-    fun toFraction(bigDecimal: BigDecimal) = bigDecimal / BigDecimal(100)
+    fun toFraction(bigDecimal: BigDecimal) = (bigDecimal.setScale(2) / BigDecimal("100.00")).setScale(2)
 
     fun asPrice(num: BigDecimal) = "£" + num.setScale(2, RoundingMode.HALF_UP).toString()
+    fun asPercentage(num: BigDecimal) = (num * BigDecimal("100.00")).setScale(2, RoundingMode.HALF_UP).toString() + "%"
 }
